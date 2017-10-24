c84533a51ea030ebc1d4201795105dfe8fbeda2e
vibindas <vibindas.m@mobme.in>
2017-10-24 18:25:39 +0530

    string reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import ("fmt"
+	"github.com/user/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hi Go !... Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG, olleH"))
 }
